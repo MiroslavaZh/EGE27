44ad462bdc5fa3941631bab315e5a17b7b567bde
Zhizdyuk <n@no.no>
2021-11-09 10:46:45 +0300

    added plan of development

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 void number_of_products_div_10()/// delitsa na 10
 {
         cout << "Hello world!" << endl;
+        /**
+        1. —читать числа
+        2. ¬се пары вывести
+        3. произведение пар
+        4. проверить делимость
+        5. вывести кол-во произведений
+
+        */
+
 }
 
 int main()
