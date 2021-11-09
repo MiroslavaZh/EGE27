8d5edb0dcbea19debb95e942d1edd85c2765a181
Zhizdyuk <n@no.no>
2021-11-09 11:04:53 +0300

    The numbers are readed and showed

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 void number_of_products_div_10()/// delitsa na 10
 {
-        cout << "Hello world!" << endl;
         /**
         1. Считать числа
-        2. Все пары вывести
+        2. Все пары вывести (первое на все, второе на все, кроме первого и т.д)
         3. произведение пар
-        4. проверить делимость
+        4. проверить делимость (оканчивается на 0)
         5. вывести кол-во произведений
-
         */
 
+        int n(0);
+        cin >> n;
+        vector<int> numbers(n,0);
+        for (int i(0); i<n; i++)
+            cin>> numbers[i];
+        for (int i(0); i<n; i++)
+            cout<< numbers[i]<< " ";
 }
 
 int main()
