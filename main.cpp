11b76134fac52233957caa2adcda7cb1cb555c28
Zhizdyuk <n@no.no>
2021-11-09 11:36:25 +0300

    only good pairs

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void number_of_products_div_10()/// delitsa na 10
         /**
         1. Считать числа+
         2. Все пары вывести (первое на все, второе на все, кроме первого и т.д)+
-        3. произведение пар
+        3. произведение пар+
         4. проверить делимость (оканчивается на 0)
         5. вывести кол-во произведений
         */
@@ -18,10 +18,15 @@ void number_of_products_div_10()/// delitsa na 10
         for (int i(0); i<n; i++)
             cin >> numbers[i];
 
-
         for(int j(0); j<n-1; j++)
             for (int i(j+1); i<n; i++)
-                cout<< numbers[j]*numbers[i] << endl;
+            {
+
+            int cur_num(numbers[j]*numbers[i]);
+            if (cur_num%10 ==0)
+                cout<< cur_num << endl;
+            }
+
 }
 
 int main()
