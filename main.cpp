092c32b5a3786b659af65e5ac2f914efd71b8ed3
Zhizdyuk <n@no.no>
2021-11-09 11:27:38 +0300

    Shows pairs

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void number_of_products_div_10()/// delitsa na 10
 {
         /**
-        1. —читать числа
+        1. —читать числа+
         2. ¬се пары вывести (первое на все, второе на все, кроме первого и т.д)
         3. произведение пар
         4. проверить делимость (оканчиваетс€ на 0)
@@ -16,9 +16,12 @@ void number_of_products_div_10()/// delitsa na 10
         cin >> n;
         vector<int> numbers(n,0);
         for (int i(0); i<n; i++)
-            cin>> numbers[i];
-        for (int i(0); i<n; i++)
-            cout<< numbers[i]<< " ";
+            cin >> numbers[i];
+
+
+        for(int j(0); j<n-1; j++)
+            for (int i(j+1); i<n; i++)
+                cout<< numbers[j]<< " " << numbers[i] << endl;
 }
 
 int main()
