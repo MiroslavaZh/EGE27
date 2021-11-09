c2d8ceffce83dbf1b9a58fc86934fb1d28361c10
Zhizdyuk <n@no.no>
2021-11-09 10:42:38 +0300

    added function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+void number_of_products_div_10()/// delitsa na 10
+{
+        cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div_10();
     return 0;
 }
