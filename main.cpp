b7960c01fe9f09db542a0a1693f33fc48b2d632c
Zhizdyuk <n@no.no>
2021-11-09 11:38:31 +0300

    number of good pairs

--- main.cpp
+++ main.cpp
@@ -18,14 +18,17 @@ void number_of_products_div_10()/// delitsa na 10
         for (int i(0); i<n; i++)
             cin >> numbers[i];
 
+        int good_pairs(0);
+
         for(int j(0); j<n-1; j++)
             for (int i(j+1); i<n; i++)
             {
 
             int cur_num(numbers[j]*numbers[i]);
             if (cur_num%10 ==0)
-                cout<< cur_num << endl;
+                good_pairs++;
             }
+                cout<< good_pairs << endl;
 
 }
 
