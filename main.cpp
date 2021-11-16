833174e9381e73ca267dc8c319731cf3585de18d
Zhizdyuk <n@no.no>
2021-11-16 11:25:39 +0300

    Checked divadability of pairs

--- main.cpp
+++ main.cpp
@@ -19,11 +19,12 @@ void max_sum_div()
     vector <int> numbers(n);
     for(int i(0);i<n;i++)
         ifs >> numbers[i];
-    cout << endl;
-    for(int j(0); j<n-1; j++)
-        for (int i(j+1); i<n; i++)
+        for(int j(0); j<n-1; j++)
+            for (int i(j+1); i<n; i++)
             {
+
             int cur_num(numbers[j]+numbers[i]);
+            if (cur_num%2!=0)
             cout<< cur_num << endl;
             }
 
