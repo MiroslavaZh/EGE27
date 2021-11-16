9a1dfa36d50f4de7b16635d2b028527d1af85e52
Zhizdyuk <n@no.no>
2021-11-16 11:09:39 +0300

    Added EFFECTIVE solution

--- main.cpp
+++ main.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void number_of_products_div_10()/// delitsa na 10
+void number_of_products_div_77()
 {
         /**
-        1. Считать числа+
-        2. Все пары вывести +
-        3. произведение пар+
-        4. проверить делимость+
-        5. вывести кол-во произведений+
+        1. Считать число
+        2. Добавить его к одному из счётчиков (n10,n5,n2,no)
+        3. Ответ:n22*n11+n22*n2+n22*no+n22(n22-1)/2*n11*n2
         */
 
-        int n(0);
+        int n(0), cur(0);
+        int n77(0), n7(0), n11(0), no(0);
         cin >> n;
-        vector<int> numbers(n,0);
-        for (int i(0); i<n; i++)
-            cin >> numbers[i];
-
-        int good_pairs(0);
-
-        for(int j(0); j<n-1; j++)
-            for (int i(j+1); i<n; i++)
-            {
-
-            int cur_num(numbers[j]*numbers[i]);
-            if (cur_num%10 ==0)
-                good_pairs++;
-            }
-                cout<< good_pairs << endl;
+        for(int i(0); i<n; i++)
+        {
+            cin >> cur;
+            if (cur%77 ==0) n77++;
+            else if (cur%7==0) n7++;
+            else if (cur%11==0) n11++;
+        }
+        no = n - n77 - n7 - n11;
+        cout << n77*n11+n77*n7+n77*no+n77*(n77-1)/2+n11*n7 << endl;
 
 }
 
 int main()
 {
-    number_of_products_div_10();
+    number_of_products_div_77();
     return 0;
 }
