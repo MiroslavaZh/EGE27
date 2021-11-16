51cfa8cf669c063481cb3a948a1cf361d1445f4d
Zhizdyuk <n@no.no>
2021-11-16 11:03:06 +0300

    Now programm takes data from file!

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 void max_sum_div()
 {
+    ifstream ifs("data.txt");
     /**
     1. Считать числа+
     2. Вывести все пары+
-    3. Вместо пары - суммы
+    3. Вместо пары - суммы+
     4. делимость сумм
     5. Только наибольшая сумма
     */
     int n(0);
-    cin >> n;
+    ifs >> n;
     vector <int> numbers(n);
     for(int i(0);i<n;i++)
-        cin >> numbers[i];
+        ifs >> numbers[i];
     cout << endl;
     for(int j(0); j<n-1; j++)
         for (int i(j+1); i<n; i++)
