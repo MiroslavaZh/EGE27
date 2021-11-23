4f9fbd12d8f307637292bfcb88420693aba2d996
Zhizdyuk <n@no.no>
2021-11-23 10:52:47 +0300

    EFFECTIVE SOLUTION!!!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void max_sum_div()
+{
+
+int n(0), max_ch(-1001), max_nech(-1001), cur_num(0);
+cin >> n;
+for (int i(0); i<n; i++)
+{
+    cin >> cur_num;
+    if (cur_num%2!=0 and cur_num>max_nech) max_nech=cur_num;
+    else if (cur_num%2==0 and cur_num>max_ch) max_ch=cur_num;
+}
+    if (max_nech!=-1001 and max_ch!=-1001) cout << max_nech+max_ch << endl;
+    else cout << -1 << endl;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
