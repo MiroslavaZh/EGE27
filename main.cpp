f73275fff0622ee47d75ceced50bb6d4c64cb9c6
ZhizdyukM <n@no.no>
2021-11-30 11:00:00 +0300

    Reads and shows numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 void min_sum_dist()
 {
     /**
-    1. Считать числа
-    2. Вывести пары
+    1. Считать числа+
+    2. Вывести пары+
     3. пары с минимальным расстоянием
     4. посчитать сумму вместо пар
     5. вывести минимум среди сум
 
     **/
+
+
+        int n(0);
+        cin >> n;
+        vector<int> numbers(n,0);
+        for (int i(0); i<n; i++)
+            cin>> numbers[i];
+        for (int i(0); i<n; i++)
+            cout<< numbers[i]<< " ";
 
 }
 
