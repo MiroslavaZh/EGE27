1224a9ca532c1bc16ecab4cbb56511dbfbcd1b77
ZhizdyukM <n@no.no>
2021-11-30 10:46:06 +0300

    Made development plan

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void min_sum_dist()
+{
+    /**
+    1. Считать числа
+    2. Вывести пары
+    3. пары с минимальным расстоянием
+    4. посчитать сумму вместо пар
+    5. вывести минимум среди сум
+
+    **/
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_dist();
     return 0;
 }
