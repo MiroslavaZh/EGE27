d0773504774ee3e40d5213d388d19c1d11815eae
ZhizdyukM <n@no.no>
2021-11-30 11:01:08 +0300

    Shows TRUE pairs

--- main.cpp
+++ main.cpp
@@ -14,13 +14,18 @@ void min_sum_dist()
     **/
 
 
-        int n(0);
-        cin >> n;
-        vector<int> numbers(n,0);
-        for (int i(0); i<n; i++)
-            cin>> numbers[i];
-        for (int i(0); i<n; i++)
-            cout<< numbers[i]<< " ";
+int n(0);
+cin >> n;
+vector<int> numbers(n,0);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
+    int min_sum(0);
+    for(int j(0); j<n-1; j++)
+        for (int i(j+3); i<n; i++)
+            cout << numbers[j] << " " << numbers[i] << endl;
+             /**int cur_num(numbers[j]+numbers[i]);
+           if (cur_num < min_sum)
+                max_sum=cur_num;**/
 
 }
 
