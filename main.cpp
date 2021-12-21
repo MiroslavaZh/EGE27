b3afe789fbb12f111a2e656601ce60bac2f8278c
ZhizdyukM <n@no.no>
2021-12-21 10:57:37 +0300

    Shows all products dividiable by 4

--- main.cpp
+++ main.cpp
@@ -23,13 +23,11 @@ void max_pr_div()
         for(int j(0); j<n-1; j++)
             for (int i(j+1); i<n; i++)
                 for (int k(i+1); k<n;k++)
-                    cout << numbers[j]*numbers[i]*numbers[k]<< endl;
-            /**{
-            int cur_num(numbers[j]*numbers[i]*numbers[i]);
-            if (cur_num%4=0 and cur_num > max_pr)
-                max_pr=cur_num;
-            }
-            cout<< max_sum << endl;*/
+                {
+                int cur_num(numbers[j]*numbers[i]*numbers[i]);
+                if (cur_num%4==0) cout<< cur_num << endl;
+                }
+
 
 }
 
