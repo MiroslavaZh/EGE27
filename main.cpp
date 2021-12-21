315829feb471fcc4042e486a54f21053a590e4ac
ZhizdyukM <n@no.no>
2021-12-21 10:41:34 +0300

    added development plan and numbers are readed

--- main.cpp
+++ main.cpp
@@ -4,6 +4,36 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void max_sum_div()
+{
+    ifstream ifs("data.txt");
+    /**
+    1. Считать числа
+    2. Вывести все тройки
+    3. Вместо тройки - произведение
+    4. делимость произведения
+    5. Только наибольшее произведение
+    */
+    int n(0);
+    cin>>n
+    vector <int> numbers(n);
+    for(int i(0);i<n;i++)
+        cin >> numbers[i];
+
+
+}
+
+
+int main()
+{
+    max_sum_div();
     return 0;
+}
+
 }
