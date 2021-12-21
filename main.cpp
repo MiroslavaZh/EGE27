49adf814827c8d2fadc3730eeb82cc35703d0b0e
ZhizdyukM <n@no.no>
2021-12-21 10:53:27 +0300

    Shows products for all TROYKY

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void max_pr_div()
         for(int j(0); j<n-1; j++)
             for (int i(j+1); i<n; i++)
                 for (int k(i+1); k<n;k++)
-                    cout << numbers[j] << " " <<  numbers[i]<< " "<< numbers[k]<< endl;
+                    cout << numbers[j]*numbers[i]*numbers[k]<< endl;
             /**{
             int cur_num(numbers[j]*numbers[i]*numbers[i]);
             if (cur_num%4=0 and cur_num > max_pr)
