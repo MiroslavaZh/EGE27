2cfd7c3c2aee0f04abd274d861c9810862f4f988
ZhizdyukM <n@no.no>
2021-12-21 11:58:40 +0300

    I GAVE UP

--- main.cpp
+++ main.cpp
@@ -19,7 +19,21 @@ void max_pr_div()
         int max_div4(0),max_nediv(0), max_nediv_no_ne_max(0), max_pr(0);
         cin >> n;
         for(int i(0); i<n; i++)
+        {
             cin >> cur;
+            if (cur%4 ==0 and cur>max_div4) max_div4=cur;
+            else if (cur%4!=0 and cur>max_nediv) max_nediv=cur;
+            else if (cur%4!=0 and cur>max_nediv_no_ne_max and max_nediv>cur)
+                {
+                    max_nediv_no_ne_max=max_nediv;
+                    max_nediv=cur;
+                }
+            int cur_num(max_div4*max_nediv*max_nediv_no_ne_max);
+            if (cur_num>max_pr)
+                max_pr=cur_num;
+
+        }
+        cout <<max_div4 << " " << max_nediv << " " << max_nediv_no_ne_max << endl;
 
 
 
