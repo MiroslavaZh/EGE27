8b2818308f657551e56437562e0693b3e3091db8
ZhizdyukM <n@no.no>
2021-12-21 11:39:02 +0300

    Reads numbers, also declared things that are needed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void max_pr_div()
+{
+
+    /**
+    1. Считать числа+
+    2. Вывести все тройки+
+    3. Вместо тройки - произведение+
+    4. делимость произведения+
+    5. Только наибольшее произведение+
+    */
+
+        int n(0), cur(0);
+        int max_div4(0),max_nediv(0), max_nediv_no_ne_max(0), max_pr(0);
+        cin >> n;
+        for(int i(0); i<n; i++)
+            cin >> cur;
+
+
+
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_pr_div();
     return 0;
 }
